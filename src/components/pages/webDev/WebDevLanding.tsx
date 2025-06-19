3820ccf28d41e419fb3e1cf378f69c1ed73fd245
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-06-19 17:11:04 +0100

    Added github links

--- src/components/pages/webDev/WebDevLanding.tsx
+++ src/components/pages/webDev/WebDevLanding.tsx
@@ -22,13 +22,17 @@ export default function WebDevLanding({lightMode}:WebDevLandingProps){
             name: "Date or Disaster",
             description: "Dating Question Game",
             src: dateOrDisaster,
-            liveDemo: "https://date-or-disaster.netlify.app/"
+            liveDemo: "https://date-or-disaster.netlify.app/",
+            github: "https://github.com/bibioyugbo/twitter-game-frontend"
+
         },
         {
             name: "Chess Multiplayer Game",
             description: "A two player chess game",
             src: chessGame,
-            liveDemo: "https://date-or-disaster.netlify.app/"
+            liveDemo: "https://date-or-disaster.netlify.app/",
+            github: "https://github.com/bibioyugbo/MultiplayerChessGame"
+
         }
     ]
 
@@ -49,8 +53,7 @@ export default function WebDevLanding({lightMode}:WebDevLandingProps){
                                         <div className={"text-sm"}>{project.description}</div>
                                     </div>
                                     <div className={"flex items-center justify-center gap-3"}>
-
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 12 12"><path fill="#fff" d="M2 12h2v-1H3v-1H2V9H1V8h1v1h1v1h1V9h1V8H3V7H2V4h1V2h1v1h3V2h1v2h1v3H8v1H6v1h1v3h2v-1h1v-1h1V3h-1V2H9V1H2v1H1v1H0v7h1v1h1Zm0 0"/></svg>
+                                        {project.github && <a target="_blank" rel="noopener noreferrer" href={project.github}>   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 12 12"><path fill="#fff" d="M2 12h2v-1H3v-1H2V9H1V8h1v1h1v1h1V9h1V8H3V7H2V4h1V2h1v1h3V2h1v2h1v3H8v1H6v1h1v3h2v-1h1v-1h1V3h-1V2H9V1H2v1H1v1H0v7h1v1h1Zm0 0"/></svg></a>}
                                     <a  target="_blank" rel="noopener noreferrer" href={project.liveDemo}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6m-7 1l9-9m-5 0h5v5"/></svg></a>
                                     </div>
                                 </div>
